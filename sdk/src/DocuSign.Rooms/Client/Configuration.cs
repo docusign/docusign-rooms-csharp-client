db70e2f9498ff430bf2a0d415bf1ede6319c75c9
root <root@devcenteradmin.docusigntest.com>
2024-11-19 07:32:38 -0800

    Version 2.0.2-v2-1.1.1 release

--- sdk/src/DocuSign.Rooms/Client/Configuration.cs
+++ sdk/src/DocuSign.Rooms/Client/Configuration.cs
@@ -26,7 +26,7 @@ namespace DocuSign.Rooms.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "2.0.1";
+        public const string Version = "2.0.2";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
